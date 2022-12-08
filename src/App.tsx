dcdff40ac4788f1a3821e7ef8dcf270ba7038891
LucasCavalheri <cav.catecumenal@gmail.com>
2022-12-08 01:05:35 -0300

    Adiciona o botão de voltar e refatora mais uma vez o calculateImc

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from './App.module.css';
+import leftArrowImage from './assets/leftarrow.png';
 import poweredImage from './assets/powered.png';
 import GridItem from './components/GridItem/GridItem';
 import { calculateImc, Level, levels } from './helpers/imc';
@@ -15,6 +16,12 @@ const App = () => {
     } else {
       alert('Digite todos os campos.');
     }
+  };
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
   };
 
   return (
@@ -60,7 +67,9 @@ const App = () => {
           )}
           {toShow && (
             <div className={styles.rightBig}>
-              <div className={styles.rightArrow}></div>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt='' width={25} />
+              </div>
               <GridItem item={toShow} />
             </div>
           )}
