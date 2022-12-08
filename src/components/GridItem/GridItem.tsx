0bbc26507ab97baaa5a9a776812f2d30acb6e7b7
LucasCavalheri <cav.catecumenal@gmail.com>
2022-12-07 22:52:01 -0300

    Feat: Adiciona o todas as informações ao grid

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -1,13 +1,28 @@
-import { Level } from "../../helpers/imc"
+import downImage from '../../assets/down.png';
+import upImage from '../../assets/up.png';
+import { Level } from '../../helpers/imc';
+import styles from './GridItem.module.css';
 
 type Props = {
-  item: Level
-}
+  item: Level;
+};
 
-const GridItem = ( { item }: Props) => {
+const GridItem = ({ item }: Props) => {
   return (
-    <div>GridItem</div>
-  )
-}
+    <div className={styles.main} style={{ backgroundColor: item.color }}>
+      <div className={styles.gridIcon}>
+        {/* {item.icon === 'up' && <img src={upImage} width="30" />}
+        {item.icon === 'down' && <img src={downImage} width="30" />} */}
+        <img src={item.icon === 'up' ? upImage : downImage} alt='' width='30' />
+      </div>
+      <div className={styles.gridTitle}>{item.title}</div>
+      <div className={styles.gridInfo}>
+        <>
+          IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+        </>
+      </div>
+    </div>
+  );
+};
 
-export default GridItem
+export default GridItem;
